a978d041477e3593f2cc8431c7366743523fcf0b
vansnoden <atagong5@gmail.com>
2024-04-24 13:44:43 +0300

    registration page ui updated

--- frontend/narrator/src/pages/auth/Register.vue
+++ frontend/narrator/src/pages/auth/Register.vue
@@ -1,41 +1,47 @@
 <template>
-    <div class="registration">
-        <!-- <video autoplay muted loop id="myVideo">
-            <source src="../assets/deepdive.mp4" type="video/mp4">
-        </video> -->
-        <v-container class="mctn">
-            <v-row>
-                <v-col cols="12" sm="10" md="6" lg="4">
-                    <h1 class="title">Join us on ADA.T <small>(PDF)</small></h1>
-                    <v-card class="mcard">
-                        <v-sheet>
-                            <v-form @submit.prevent="signup">
-                                <h5 class="mtitle">Join us, create an account here.</h5>
-                                <v-text-field type="text" label="username" v-model="username"></v-text-field>
-                                <v-text-field type="text" label="full name" v-model="fullname"></v-text-field>
-                                <v-text-field type="email" label="email" v-model="email"></v-text-field>
-                                <v-text-field type="password" label="password" v-model="password"></v-text-field>
-                                <v-text-field type="password" label="confirm password" v-model="cpassword"></v-text-field>
-                                <v-btn color="primary" variant="text" type="submit">Submit</v-btn>
-                                <div class="info-box">
-                                    <span>Already have an account?</span>
-                                    <router-link to="/login">log in here.</router-link>
-                                </div>
-                                <div class="errors" v-if="errors">
-                                    😢 
-                                    <span>{{ errors }}</span>
-                                </div>
-                            </v-form>
-                        </v-sheet>
-                    </v-card>
-                </v-col>
-            </v-row>
-        </v-container>
-    </div>
+    <AuthLayout>
+        <template v-slot:content>
+            <v-container class="registration-ctn">
+                <v-row class="mctn">
+                    <v-col cols="12" sm="12" md="4">
+                        <v-form @submit.prevent="signup">
+                            <v-card class="mcard">
+                                <v-card-title>
+                                    <h5 class="title">Welcome to ADA_T (PDF)</h5>
+                                </v-card-title>
+                                <v-card-text>
+                                    <h5 class="mtitle">Join us, create an account here.</h5>
+                                    <v-text-field type="text" label="username" v-model="username"></v-text-field>
+                                    <v-text-field type="text" label="full name" v-model="fullname"></v-text-field>
+                                    <v-text-field type="email" label="email" v-model="email"></v-text-field>
+                                    <v-text-field type="password" label="password" v-model="password"></v-text-field>
+                                    <v-text-field type="password" label="confirm password" v-model="cpassword"></v-text-field>
+                                    
+                                    <div class="errors" v-if="errors">
+                                        😢 
+                                        <span>{{ errors }}</span>
+                                    </div>
+                                </v-card-text>
+                                <v-card-actions class="actions">
+                                    <v-btn color="primary" variant="text" type="submit">Submit</v-btn>
+                                    <div class="info-box">
+                                        <span>Already have an account?</span>
+                                        <router-link to="/login">log in here.</router-link>
+                                    </div>
+                                </v-card-actions>
+                            </v-card>
+                        </v-form>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </template>
+    </AuthLayout>
 </template>
 
 <script>
 import { useUserStore } from "@/stores/UserStore";
+import AuthLayout from "./partials/AuthLayout.vue";
+
 export default {
     setup() {
         const userStore = useUserStore();
@@ -78,8 +84,7 @@ export default {
 
 
 <style lang="scss">
-.registration{
-    background-color: skyblue;
+.registration-ctn{
     height:100%;
     display: flex;
     flex-direction: row;
@@ -89,24 +94,12 @@ export default {
     .mcard{
         padding: 1em!important;
     }
-    // #myVideo {
-    //     position: fixed;
-    //     right: 0;
-    //     bottom: 0;
-    //     min-width: 100%;
-    //     min-height: 100%;
-    //     z-index: 1;
-    // }
-    // .title{
-    //     color:#fff;
-    // }
     .mtitle{
         font-weight: 1.5rem;
         margin-bottom: 1em;
     }
     .mctn{
         margin: auto!important;
-        z-index: 5;
         height:100%;
         display: flex!important;
         flex-direction: row!important;
@@ -122,10 +115,18 @@ export default {
         border: 1px solid lightgrey;
         border-radius: 5px;
         font-size: 0.8rem;
+        span{
+            margin-right: 1em;
+        }
     }
     .errors{
         margin-top: 2em;
         color: red;
+    }
+    .actions{
+        display: flex;
+        flex-direction: column;
+        gap:1em;
     }
 }
 </style>
